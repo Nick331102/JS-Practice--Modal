25c5d87f300601d7f89f76835e6d798ddee586db
Nick331102 <75483104+Nick331102@users.noreply.github.com>
2021-12-03 14:02:23 -0500

    Add files via upload

--- modal.js
+++ modal.js
@@ -1,7 +1,11 @@
 const modalOpen = document.querySelector(".open")
 const modalContainer = document.querySelector(".modal-container")
-const close = document.querySelector(".modal-btn")
+const modalClose = document.querySelector(".modal-btn")
 
 modalOpen.addEventListener("click", () => {
     modalContainer.classList.add("show")
 })
+modalClose.addEventListener("click", () => {
+    modalContainer.classList.remove("show")
+})
+
